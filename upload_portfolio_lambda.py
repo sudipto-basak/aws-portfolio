7ce9d896808c5cbb655784448962d13b6b7ccdca
Sudipto Basak <Sudipto@Sudiptos-MacBook-Pro.local>
2019-01-27 03:32:00 -0500

    code for lambda added

---
+++ upload_portfolio_lambda.py
@@ -0,0 +1,19 @@
+from io import BytesIO
+import zipfile
+import boto3
+import mimetypes
+
+session = boto3.Session(profile_name='pythondev')
+s3 = session.resource('s3')
+build_bucket = s3.Bucket('portfolio.build.sudiptobasak')
+portfolio_bucket = s3.Bucket('portfolio.sudiptobasak')
+
+portfolio_zip = BytesIO()
+build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+
+with zipfile.ZipFile(portfolio_zip) as myzip:
+    for name in myzip.namelist():
+        obj = myzip.open(name)
+        portfolio_bucket.upload_fileobj(obj, name,
+        ExtraArgs={'ContentType': mimetypes.guess_type(name)[0]})
+        portfolio_bucket.Object(name).Acl().put(ACL='public-read')
