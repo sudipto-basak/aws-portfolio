6fc518d5650e0e9f34588864ff1bb85bbded77dc
Sudipto Basak <Sudipto@Sudiptos-MacBook-Pro.local>
2019-01-27 13:57:11 -0500

    Update lambda function

--- upload_portfolio_lambda.py
+++ upload_portfolio_lambda.py
@@ -3,17 +3,20 @@ import zipfile
 import boto3
 import mimetypes
 
-session = boto3.Session(profile_name='pythondev')
-s3 = session.resource('s3')
-build_bucket = s3.Bucket('portfolio.build.sudiptobasak')
-portfolio_bucket = s3.Bucket('portfolio.sudiptobasak')
 
-portfolio_zip = BytesIO()
-build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+def lambda_handler(event, context):
 
-with zipfile.ZipFile(portfolio_zip) as myzip:
-    for name in myzip.namelist():
-        obj = myzip.open(name)
-        portfolio_bucket.upload_fileobj(obj, name,
-        ExtraArgs={'ContentType': mimetypes.guess_type(name)[0]})
-        portfolio_bucket.Object(name).Acl().put(ACL='public-read')
+    s3 = boto3.resource('s3')
+    build_bucket = s3.Bucket('portfolio.build.sudiptobasak')
+    portfolio_bucket = s3.Bucket('portfolio.sudiptobasak')
+
+    portfolio_zip = BytesIO()
+    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+        for name in myzip.namelist():
+            obj = myzip.open(name)
+            portfolio_bucket.upload_fileobj(obj, name, ExtraArgs={'ContentType': mimetypes.guess_type(name)[0]})
+            portfolio_bucket.Object(name).Acl().put(ACL='public-read')
+
+    return
